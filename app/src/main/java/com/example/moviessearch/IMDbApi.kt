43fc30d74e7005d04924d0d18460304aa53d494f
sergeyboykov <sutemi67@gmail.com>
2024-06-27 09:33:35 +0300

    working app

--- app/src/main/java/com/example/moviessearch/IMDbApi.kt
+++ app/src/main/java/com/example/moviessearch/IMDbApi.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface IMDbApi {
-    @GET("/en/API/SearchMovie/API_KEY/{expression}")
+    @GET("/en/API/SearchMovie/k_zcuw1ytf/{expression}")
     fun findMovie(@Path("expression") expression: String): Call<MoviesResponse>
 }
